f7aff8b768888af2d9d6cdb2671fe0a19afa16ee
kenjidoom <vidodevcool@protonmail>
2022-08-01 15:32:18 -0700

    threading code for proxies downloading added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from colorama import Fore
 from time import time
+import concurrent.futures
 import requests
 import wget
 import os
@@ -7,7 +8,8 @@ import os
 def grabproxies(url):
     file_name_start_pos = url.rfind("/") + 1
     file_name = url[file_name_start_pos:]
-
+    
+    # Multiprocessing code
     r = requests.get(url, stream=True)
     if r.status_code == requests.codes.ok:
         print(Fore.GREEN + "Downloaded: " + url)
@@ -17,11 +19,18 @@ def grabproxies(url):
     elif r.status_code == 404:
         print(Fore.RED + "404 Failed to download: " + url)
 
+def download_proxies():
+    urls = ['https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt',
+    'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt',
+    'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt']
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = executor.map(grabproxies, urls)
+
 def testproxies():
     pass
 
-
-grabproxies("https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txtgt")
-grabproxies("https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt")
-grabproxies("https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt")
-
+# Thanks to TheSpeedX
+#grabproxies("https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txtgt")
+#grabproxies("https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt")
+#grabproxies("https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt")
+download_proxies()
