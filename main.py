224d0f3bde6394c706afa2d06d510115808ed43e
KenjiDoom <vidodevcool@protonmail>
2022-08-01 18:29:51 -0700

    proxy_checker function, checking if proxies are valid or invalid

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import requests
 import wget
 import os
 
-def grabproxies(url):
+def grab_proxies(url):
     # The main work function
     file_name_start_pos = url.rfind("/") + 1
     file_name = url[file_name_start_pos:]
     
-
     r = requests.get(url, stream=True)
     if r.status_code == requests.codes.ok:
         print(Fore.GREEN + "Downloaded: " + url)
@@ -25,14 +24,35 @@ def download_proxies():
     urls = ['https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt',
     'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt',
     'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt']
+
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = executor.map(grabproxies, urls)
+        results = executor.map(grab_proxies, urls) # pool map method
 
-def testproxies():
-    pass
+def proxy_checker(filename):
+    with open(filename, 'r') as IP:
+        for proxy in IP:
+            try:
+                proxy = proxy.strip()
+                r = requests.get('https://httpbin.org/ip', proxies={'http': proxy, 'https':proxy}, timeout=1)
+                print(Fore.RED + proxy)
+            except requests.ConnectionError as err:
+                    print(Fore.GREEN + repr(err))
 
-# Thanks to TheSpeedX
-#grabproxies("https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txtgt")
-#grabproxies("https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt")
-#grabproxies("https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt")
-download_proxies()
+# Self-note:
+# How to know what file is being used? 
+# to save to a socks5, socks4 working file
+
+
+def test_proxies():
+    filename = ['/home/$USER/Desktop/Proxy-Checker/socks5.txt',
+    '/home/$USER/Desktop/Proxy-Checker/socks4.txt',
+    '/home/$USER/Desktop/Proxy-Checker/http.txt']
+    # Self-Note: Grab the users path for the .txt files
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = executor.map(proxy_checker, filename)
+
+
+
+test_proxies()
+#download_proxies()
